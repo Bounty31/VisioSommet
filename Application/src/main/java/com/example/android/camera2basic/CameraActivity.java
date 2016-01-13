bdaf60189c306c9abe1d2257ae907c81f187b1ab
Awnoa <zerouali.bardai.omar@gmail.com>
2016-01-13 15:43:42 +0100

    Ajout localisation

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -16,10 +16,25 @@
 
 package com.example.android.camera2basic;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements LocationListener {
+
+    final int PERMISSION_LOCATION = 3;
+    private double latitude = -1;
+    private double longitude = -1;
+    private Location locationToBear = null;
 
     @Override
     protected void onPause() {
@@ -30,13 +45,92 @@ public class CameraActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        latitude = 47.642787;
+        longitude = 6.8397398;
+        locationToBear = new Location("Current");
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2BasicFragment.newInstance())
                     .commit();
         }
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
 
-        // MDR etrtr
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    //Cela signifie que la permission à déjà était
+                    //demandé et l'utilisateur l'a refusé
+                    //Vous pouvez aussi expliquer à l'utilisateur pourquoi
+                    //cette permission est nécessaire et la redemander
+                } else {
+                    //Sinon demander la permission
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_LOCATION);
+                }
+            }
+            else{
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(Context.LOCATION_SERVICE);
+
+                LocationListener locationListener = this;
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, 100, 0, locationListener);
+            }
+        } else {
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            LocationListener locationListener = this;
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 100, 0, locationListener);
+
+        }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("Localisation", location.getLatitude() + "," + location.getLongitude());
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationManager locationManager = (LocationManager)
+                            getSystemService(Context.LOCATION_SERVICE);
+
+                    LocationListener locationListener = this;
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER, 100, 0, locationListener);
+                } else {
+                    // La permission est refusée
+                }
+                return;
+            }
+        }
+    }
 }
+
+
